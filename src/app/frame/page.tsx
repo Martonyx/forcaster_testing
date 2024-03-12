e1915484f8d4da77f1b6c6824b8ecdec0b7bdb7b
Martonyx <martinchukwuneme24@gmail.com>
2024-03-12 07:56:58 +0100

    first commit

--- src/app/frame/page.tsx
+++ src/app/frame/page.tsx
@@ -6,14 +6,15 @@ export const metadata: Metadata = {
     openGraph: {
       title: 'DragonBaldZ Frame',
       description: 'A Farcaster Frame for DragonBaldZ',
-      images: [`https://dragonbaldz.vercel.app/dragonbaldz.jpg`],
+      images: [`http://dragonbaldz.vercel.app/dragonbaldz.jpg`],
     },
     other: {
       'fc:frame': 'vNext',
-      'fc:frame:image': `https://dragonbaldz.vercel.app/dragonbaldz.jpg`,
-      'fc:frame:image:aspect_ratio': '1.91:1',
-      'fc:frame:post_url': `https://dragonbaldz.vercel.app/api/basic`,
+      'fc:frame:image': `http://dragonbaldz.vercel.app/dragonbaldz.jpg`,
+      'fc:frame:image:aspect_ratio': '1:1',
+      'fc:frame:post_url': `https://uplink.wtf/bamehouse/mintboard`,
       'fc:frame:button:1': 'Mint R.I.P AKIRA',
+      "fc:frame:button:1:action": "link",
     },
   }
 
