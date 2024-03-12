c860f771286ef3c750f6b6f782a8a4d903f36280
Martonyx <martinchukwuneme24@gmail.com>
2024-03-12 21:07:11 +0100

    first commit

--- src/app/frame/page.tsx
+++ src/app/frame/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
       'fc:frame:post_url': `http://dragonbaldz.vercel.app`,
       'fc:frame:button:1': 'Mint R.I.P AKIRA',
       "fc:frame:button:1:action": "link",
-      "fc:frame:button:1:target": "https://zora.co/collect/base:0xf5a706cc0b797dbc51be69d891bd22ac8f6edb6a/premint-1" 
+      "fc:frame:button:1:target": "https://zora.co/collect/base:0xf5a706cc0b797dbc51be69d891bd22ac8f6edb6a/" 
     },
   }
 
